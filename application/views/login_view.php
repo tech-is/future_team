6a22f1522c1a96752be19db4c51170323030aad4
taishi0530 <11170795@g.matsuyama.ac.jp>
2019-11-29 20:27:52 +0900

    新しいプログラム

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,32 +1,17 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
+    echo doctype();
+    include(dirname(__FILE__)."/include/header.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- bootstrap読み込み -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- css読み込み -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/MtoF_form.css">
-    <!-- google_fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Fascinate+Inline|Mansalva&display=swap" rel="stylesheet">
-    <title>ログイン</title>
-</head>
 <body>
-
     <div class="header-container">
         <h1>Log In<h1>
     </div>
     <div style="text-align:right;">
-        <a href="<?php echo base_url(); ?>MtoF_sign_up/">新規登録</a>
+        <a href="<?php echo base_url(); ?>Main_ctrl/view_sign_up/">新規登録</a>
     </div>
     <div class="body-container">
-        <form id="form" onsubmit="return false;">
+        <form id="form" onsubmit="return false;" class = "col-sm-12">
             <div class="form-group col-sm-12">
                 <p>名前</p>
                 <input class="form-control form-container" type="text" name="name" id="name" placeholder="(例)ヤマダタロウ" pattern="[ァ-ヶ]*" title="全角カナで入力してください" required><br>
@@ -41,12 +26,14 @@
             </div>
             <div class="form-group col-sm-12">
                 <!-- CSRF対策 「トークンの埋め込み」 -->
-                <input type="hidden" name="token" id="token" value="<?php echo $_SESSION['token']; ?>"> 
+                <input type="hidden" name="<?php echo $csrf_token;?>" id="token" value="<?php echo $csrf_hash;?>" />
                 <input class="btn btn-outline-primary" type="submit" value="ログイン" >
             </div>
         </form>
     </div>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script type="text/javascript" src= "<?php echo base_url();?>assets/js/login_ajax.js"></script>
+    <script type="text/javascript" src= "<?php echo base_url();?>assets/js/sign_in.js"></script>
+    <!-- sweet_alert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 </body>
 </html>
--- application/views/mypage_view.php
+++ application/views/mypage_view.php
@@ -1,28 +1,10 @@
 <?php 
     defined('BASEPATH') OR exit('No direct script access allowed');
-     // sessionが開始していない場合sessionを開始する
-    if(!isset($_SESSION['id'])){
-        echo 'ログイン失敗';
-    }
+    echo doctype();
+    include(dirname(__FILE__)."/include/header.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- bootstrap読み込み -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- css読み込み -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/MtoF_form.css">
-    <!-- google_fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Fascinate+Inline|Mansalva&display=swap" rel="stylesheet">
-    <title>マイページ</title>
-</head>
 <body>
-<form action="/MtoF/view_form">
+<form action="/Form_ctrl">
     <button class="btn btn-outline-primary" >未来へメールを送る</button>
 </form>
 <?php
@@ -32,6 +14,6 @@
     echo $name."<br>".$mail_address."<br>".$delete_flag;
     // session_destory
     ?>
-<form action="/MtoF_login/logout">
+<form action="/login_ctrl/logout">
     <button class="btn btn-outline-primary" >ログアウト</button>
 </form>
